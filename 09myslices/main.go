6017f5fa2a93f2dae3b84346b083bec95af14158
shahan <shahan.khaan@gmail.com>
2024-07-02 00:35:15 +0500

    covered the slices list

---
+++ 09myslices/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	fmt.Println("Welcome to slices in golang")
+
+	var fruits = []string{"Apple", "Mango", "Peach"}
+	fmt.Printf("Type of fruits is %T \n", fruits)
+
+	fruits = append(fruits, "Banana", "Grapes", "Orange")
+	fmt.Println(fruits)
+
+	fruits = append(fruits[1:3], "Apple")
+	fmt.Println(fruits)
+
+	highScores := make([]int, 4)
+	highScores[0] = 234
+	highScores[1] = 945
+	highScores[2] = 465
+	highScores[3] = 867
+
+	highScores = append(highScores, 555, 666, 321)
+
+	fmt.Println(highScores)
+
+	fmt.Println(sort.IntsAreSorted((highScores)))
+	sort.Ints(highScores)
+	fmt.Println(highScores)
+}
