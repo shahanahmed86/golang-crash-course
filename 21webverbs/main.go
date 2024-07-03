fcec27ab3a449c8a8de5ba4877a41d9f11a21204
shahan <shahan.khaan@gmail.com>
2024-07-03 19:44:42 +0500

    covered the web verbs with json data

--- 21webverbs/main.go
+++ 21webverbs/main.go
@@ -10,9 +10,39 @@ import (
 func main() {
 	fmt.Println("Welcome to web verbs in golang")
 	PerformGetRequest("/get")
+	PerformPostJsonRequest("/post")
 }
 
 const baseUrl string = "http://localhost:3000"
+
+func PerformPostJsonRequest(route string) {
+	myUrl := baseUrl + route
+
+	// fake json payload
+	requestBody := strings.NewReader(`
+		{
+			"coursename": "Let's go with golang",
+			"price": 0,
+			"platform": "learncodeonline.in"
+		}
+	`)
+
+	resp, err := http.Post(myUrl, "application/json", requestBody)
+	checkNilErr(err)
+
+	defer resp.Body.Close()
+
+	var responseString strings.Builder
+	content, contentErr := io.ReadAll(resp.Body)
+	checkNilErr(contentErr)
+
+	responseString.Write(content)
+
+	fmt.Println("Status code is:", resp.StatusCode)
+	fmt.Println("Content length is:", resp.ContentLength)
+
+	fmt.Println("Data inside the response is:", responseString.String())
+}
 
 func PerformGetRequest(route string) {
 	myUrl := baseUrl + route
